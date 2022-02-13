4fd60e914a41f2f1b7a835ec1958af48079dc196
AGESCI Catania 14 <98856287+agescicatania14@users.noreply.github.com>
2022-02-13 18:29:45 +0100

    Add files via upload

--- javascript/topFunction.js
+++ javascript/topFunction.js
@@ -1,10 +1,10 @@
 var mybutton = document.getElementById("topBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
+// When the user scrolls down 1000px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+  if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
